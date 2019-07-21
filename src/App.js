e0d2b03186178f1714e681c08f33f50c9386433b
karinfdez <karinfdez@yahoo.com>
2019-07-21 12:55:49 -0400

    Passing props to components. Destructuring props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-const App = () => {
+const Pet = (props) => {
+    const { name, animal, breed } = props;
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {}, "Adopt Me!")
+        [ 
+            React.createElement("h1", {}, name),
+            React.createElement("h2", {}, animal),
+            React.createElement("h2", {}, breed)
+        ]
+    );
+}
+const App = () => {
+    return React.createElement(
+        "div",
+        {id: "something-important"},
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, { 
+                name: "Luna", 
+                animal: "Dog", 
+                breed: "Havanese" 
+            }),
+            React.createElement(Pet,{ 
+                name: "Ginger", 
+                animal: "Cat", 
+                breed: "Mixed" 
+            })
+        ]
     );
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
