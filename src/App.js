7d115eac96b390d5b492bdeb2f8504faba274646
karinfdez <karinfdez@yahoo.com>
2019-07-24 20:59:33 -0400

    Add JSX

--- src/App.js
+++ src/App.js
@@ -3,18 +3,12 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Ginger",
-      animal: "Cat",
-      breed: "Mixed"
-    })
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Ginger" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,10 @@
 import React from "react";
-
-export default function Pet(props) {
-  const { name, animal, breed } = props;
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
+export default function Pet({ name, animal, breed }) {
+  return (
+    <div>
+      <h1>{name.toUpperCase()}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
