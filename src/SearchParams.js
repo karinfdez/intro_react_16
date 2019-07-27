145b52899e965002ffb3c48f4de03a790bfc63a0
karinfdez <karinfdez@yahoo.com>
2019-07-27 16:52:48 -0400

    Add a select to show list of animals

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   //location = current state
   //setLocation = updated function for a particular piece of state(location, in this example)
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+
   return (
     <div className="search-params">
       <form>
@@ -15,6 +18,23 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={e => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {/*() -> Implicit return.It's like saying {return ....}*/}
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
