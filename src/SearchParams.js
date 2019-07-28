16dc4d4474850c5e28a395805c3e7708fefe6426
karinfdez <karinfdez@yahoo.com>
2019-07-28 13:19:47 -0400

    Add effect dependency, pass parameters that decide when execute effect

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropDown";
+
 const SearchParams = () => {
   //location = current state
   //setLocation = updated function for a particular piece of state(location, in this example)
   const [location, setLocation] = useState("Seattle, WA");
-  // const [animal, setAnimal] = useState("dog");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropDown] = useDropdown("animal", "dog", ANIMALS);
-  const [breed, BreedDropDown] = useDropdown("breed", "", breeds);
+  const [breed, BreedDropDown, setBreed] = useDropdown("breed", "", breeds);
+  //useEffect takes place of several clicles hooks
+  //(componentDidMount, componentWillUnmount, componentDidUpdate)
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [setBreeds, setBreed, animal]); //If any of these parameters changes, rerun useEffect after comp renders. Order of parameters don't matter
 
   return (
     <div className="search-params">
