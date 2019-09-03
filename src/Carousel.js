34701a69478aad621d64ead937a55f397f957454
karinfdez <karinfdez@yahoo.com>
2019-09-02 20:52:33 -0400

    Add some modifications

--- src/Carousel.js
+++ src/Carousel.js
@@ -9,29 +9,31 @@ class Carousel extends React.Component {
     };
   }
 
-  //arrow functions do not create context when they are invoked(avoids binding the function to 'this')
-  //use arrow functions with event listeners and functions passing down into childrens
-  handleIndexClick = event => {
-    this.setState({ active: +event.target.dataset.index }); //plus sign will turn the string into number
-  };
-
   static getDerivedStateFromProps({ media }) {
-    let photos = ["https://placecorgi.com/600/600"];
+    let photos = ["http://placecorgi.com/600/600"];
 
-    if (media.lenght) {
+    if (media.length) {
       photos = media.map(({ large }) => large);
     }
+
     return { photos };
   }
 
+  //arrow functions do not create context when they are invoked(avoids binding the function to 'this')
+  //use arrow functions with event listeners and functions passing down into childrens
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index
+    });
+  };
   render() {
     const { photos, active } = this.state;
-
     return (
       <div className="carousel">
         <img src={photos[active]} alt="animal" />
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
+            // eslint-disable-next-line
             <img
               key={photo}
               onClick={this.handleIndexClick}
